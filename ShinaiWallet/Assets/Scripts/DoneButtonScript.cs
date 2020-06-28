c2d181aee72a5fe39b64b4188dcd7247f25ecc40
Bifrost19 <miroto1234@abv.bg>
2020-06-28 17:11:45 +0300

    Add words and PINcode scene

---
+++ ShinaiWallet/Assets/Scripts/DoneButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoneButtonScript : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        SceneManager.LoadScene("Pincodescene");
+    }
+}
