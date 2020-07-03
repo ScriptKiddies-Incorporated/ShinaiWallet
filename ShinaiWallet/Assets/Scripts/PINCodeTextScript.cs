d93e964512c86957e4c9e0ffeecd732c36c1eb93
Bifrost19 <miroto1234@abv.bg>
2020-07-03 18:47:48 +0300

    Add confirm PINcode and main wallet page scene

---
+++ ShinaiWallet/Assets/Scripts/PINCodeTextScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PINCodeTextScript : MonoBehaviour
+{
+    public static Text pinCode;
+    public static string pinCodeString;
+    
+    public void TextInput()
+    {
+        
+        pinCode.text = pinCodeString;
+       // pinCodeString.Replace(pinCodeString,"1");
+    }
+}
