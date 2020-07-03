5b71584e0217c56173f74f65499dbcfdfd4fcf8b
Relaxo143 <relaxo143@protonmail.com>
2020-07-03 19:25:44 +0300

    Generate 24 word seed

--- ShinaiWallet/Assets/Scripts/NewWallet.cs
+++ ShinaiWallet/Assets/Scripts/NewWallet.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using NBitcoin;
 
 public class NewWallet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-		// Bitcoin.
-		var key = new Key();
-	 
-    }
+	// Start is called before the first frame update
 
-	void OnButtonClick()
+	public static NBitcoin.Network network = NBitcoin.Network.TestNet;
+	void Start()
 	{
-		var key = new Key();
+		// Bitcoin.
+		string[] wordsArray = BIP39.GetWordListArray();
+		Wordlist wordList = new Wordlist(wordsArray, ' ', "m1");
+		Mnemonic m1 = new Mnemonic(wordList);
+	}
+
+   void OnButtonClick()
+	{
+		//var key = new Key();
 	}
 
     // Update is called once per frame
@@ -24,3 +28,4 @@ public class NewWallet : MonoBehaviour
         
     }
 }
+
